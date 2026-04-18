d93cfbf2f8f45591ab476d768f9aaa41ad23eb1e
Zubair Hassan <zubairhassan652@gmail.com>
2026-04-18 16:31:58 +0500

    root url added

--- helm_app/views.py
+++ helm_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import JsonResponse
+from django.views.generic import View
+
+class HealthCheckView(View):
+    def get(self, request):
+        return JsonResponse({'status': 'ok'})
--- helm_charts/urls.py
+++ helm_charts/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_prometheus.urls')),
+    path('', include('helm_app.urls')),
 ]
